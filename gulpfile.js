61e228563f191b78316a343031ef9b3fc6c3a008
Ashley Narcisse <ashlay49@gmail.com>
2016-05-20 14:02:04 -0400

    updated build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
+  rename = require('gulp-rename'),
   clean = require('gulp-clean-css');
 
 gulp.task('build', function(){
   return gulp.src('./src/main.scss')
     .pipe(sass())
-    .pipe(clean())
-    .pipe(gulp.dest('./build'));
+    .pipe(rename('halp.css'))
+    .pipe(gulp.dest('./build'))
+    ;
 });
 
-gulp.task('default',['build']);
+gulp.task('minify', function(){
+  return gulp.src('./src/main.scss')
+    .pipe(sass())
+    .pipe(clean())
+    .pipe(rename('halp.min.css'))
+    .pipe(gulp.dest('./build'))
+    ;
+});
+
+gulp.task('default',['build', 'minify']);
